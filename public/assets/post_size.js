2edd6c8b2ccd74ececcfd22733443f2b0629e0fe
pavel demyanov <pavel@demjanov.ru>
2025-04-02 11:35:25 +0300

    Настройте линтер
    - [] настроил линтер
    - [] подключил mocha глобально
    - [] macha как плагин для eslint
    - [] исправил все замечения eslint
    - [] исправил Readme.md
    
    https://edu.burtovoy.org/mod/assign/view.php?id=4545

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,25 +1,25 @@
 import isEmodzy from './is_emodzy.js';
 
 const postSize = (post) => {
-    let count = 0;
-    let i = 0;
-    const n = post.length;
+  let count = 0;
+  let i = 0;
+  const n = post.length;
 
-    while (i < n) {
-        const isHttpLink = post.startsWith('http://', i) || post.startsWith('https://', i);
-        const isWwwLink = post.startsWith('www.', i);
+  while (i < n) {
+    const isHttpLink = post.startsWith('http://', i) || post.startsWith('https://', i);
+    const isWwwLink = post.startsWith('www.', i);
 
-        if (isHttpLink || isWwwLink) {
-            while (i < n && post[i] !== ' ') {
-                i++;
-            }
-        } else {
-            i += isEmodzy(post[i]) ? 2 : 1;
-            count++;
-        }
+    if (isHttpLink || isWwwLink) {
+      while (i < n && post[i] !== ' ') {
+        i += 1;
+      }
+    } else {
+      i += isEmodzy(post[i]) ? 2 : 1;
+      count += 1;
     }
+  }
 
-    return count;
+  return count;
 };
 
 export default postSize;
